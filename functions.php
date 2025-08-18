7acdd09c5e7ae2a256e1cf923c94fec4b65f4b41
NinjaPhilip <philip@ninjasforhire.co.za>
2025-08-18 20:16:45 +0200

    Initial commit - Star Master theme with GitHub Updater support

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Star Master Theme Functions
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Include ACF fields
+require_once get_template_directory() . '/includes/acf-fields.php';
---
+++ includes/acf-fields.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * ACF Fields Registration
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
